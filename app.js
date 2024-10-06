25d5f34961a73a34845d5b5630aa890495f8c990
Dilan kanishka <dilankanishka032@gmail.com>
2024-10-06 17:36:19 +0530

    update port number (port number  add into  .env file)

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const app = express(); // Create an instance of an Express app
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-
-app.listen(3000,()=>{
-    console.log("Sever Start on Port 3000");
+// port number can change in (.env) file and defalt port number 3000
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`Sever Start on Port ${port}`);
 });
 
 //define routes
--- routes/page.js
+++ routes/page.js
@@ -4,11 +4,15 @@ const router = express.Router();
 // define controllers
 const pinLoginController = require('../controllers/auth/pinLoginController');
 const nameLoginController = require('../controllers/auth/nameLoginController');
+const logOutController = require('../controllers/auth/logOutController');
 
 
 // for login (pinLogin) and (user name & password login)
 router.post('/pinLogin', pinLoginController.pinLogin );
 router.post('/userNameLogin', nameLoginController.userNameLogin );
+
+// for log out
+//router.post('/logOut', logOutController.logOut );
 
 
 
