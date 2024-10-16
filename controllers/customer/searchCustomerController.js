2a485f7a27aa2814995bfd3f3ea25fc7a92ec5a1
Dilan kanishka <dilankanishka032@gmail.com>
2024-10-16 12:05:43 +0530

    develop search customers end-point

---
+++ controllers/customer/searchCustomerController.js
@@ -0,0 +1,27 @@
+const db = require('../../database');
+
+//module for get all Customer details
+exports.searchCustomer = (req,res) =>{
+    const user = req.result; // this data get from Json web token
+    const userInput = req.body.Name; // get user input from user
+    
+    // Check if userInput is provided, otherwise send an error
+    if (!userInput) {
+        return res.status(400).json({ message: 'Please provide a name to search' });
+    }
+
+    // get all details from cussup table in CS
+    db.query('SELECT * FROM cussup WHERE cussupcat_id = ? AND Name LIKE ?', ['CS', `%${userInput}%`],(error, result) =>{
+        if (error) {
+            return res.status(500).json({ message: 'Server error, please try again later'  });
+        }
+
+        if (result.length === 0) {
+            return res.status(404).json({ message: 'Customer details not found' }); 
+        }
+
+        // Return all customer details (send entire result array to the client)
+        res.status(200).json(result);
+        
+    });
+};
--- routes/customer.js
+++ routes/customer.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 //define controller
 const allCustomerDetailsController = require('../controllers/customer/allCustomerDetailsController');
+const searchCustomerController = require('../controllers/customer/searchCustomerController');
 
 // get all customer details
 router.get('/all-details', allCustomerDetailsController.allCustomerDetails);
+router.get('/search', searchCustomerController.searchCustomer);
 
 
 // Export the router
