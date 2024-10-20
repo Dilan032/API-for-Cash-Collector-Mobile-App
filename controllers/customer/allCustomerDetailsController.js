6aa99d4f9c7a5a26d32ddac205dd5bc88a4cfa60
Dilan kanishka <dilankanishka032@gmail.com>
2024-10-20 15:49:48 +0530

    get and search customer data and verify token

--- controllers/customer/allCustomerDetailsController.js
+++ controllers/customer/allCustomerDetailsController.js
@@ -3,6 +3,22 @@ const db = require('../../database');
 //module for get all Customer details
 exports.allCustomerDetails = (req,res) =>{
     const user = req.result; // this data get from Json web token
+
+    // Check if 'user' and 'user.UserName' are defined before querying the database
+    if (!user || !user.UserName) {
+        return res.status(400).json({ message: 'Invalid token or missing UserName' });
+    }
+
+    // Check if the user exists in the database
+    db.query('SELECT * FROM users WHERE UserName = ?', [user.UserName],(error, result) =>{
+        if (error) {
+            return res.status(500).json({ message: 'Server error, please try again later'  });
+        }
+
+        if (result.length === 0) {
+            return res.status(404).json({ message: 'User not found' }); 
+        }        
+    });// end query 
     
     // get all details from cussup table in CS
     db.query('SELECT * FROM cussup WHERE cussupcat_id = ?', ['CS'],(error, result) =>{
--- routes/customer.js
+++ routes/customer.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
 //define controller
 const allCustomerDetailsController = require('../controllers/customer/allCustomerDetailsController');
 const searchCustomerController = require('../controllers/customer/searchCustomerController');
 
 // get all customer details
-router.get('/all-details', allCustomerDetailsController.allCustomerDetails);
-router.get('/search', searchCustomerController.searchCustomer);
+router.get('/all-details', auth,allCustomerDetailsController.allCustomerDetails);
+router.get('/search', auth,searchCustomerController.searchCustomer);
 
 
 // Export the router
