01982facfe1b5303880cc1d9d3db612c70dc46a5
Dilan kanishka <dilankanishka032@gmail.com>
2024-11-19 13:37:02 +0530

    develop cash collect end-point

--- routes/collectAmount.js
+++ routes/collectAmount.js
@@ -3,13 +3,18 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 
 //define controller
-const collectAmountFromCustomer = require('../controllers/collectAmount/fromCustomerController');
+// const collectAmountFromCustomer = require('../controllers/collectAmount/fromCustomerController');
+
+const cashController = require('../controllers/collectAmount/cashController');
 const showTodayCollection = require('../controllers/collectAmount/showTodayCollection');
 
 
 
 // store customer payment details
-router.post('/all', auth,collectAmountFromCustomer.collectAmount);
+// router.post('/all', auth,collectAmountFromCustomer.collectAmount);
+
+
+router.post('/cashCollect', auth,cashController.cashCollect);
 
 router.get('/dayCollection/:EmpCode', auth,showTodayCollection.showTodayCollection);
 
